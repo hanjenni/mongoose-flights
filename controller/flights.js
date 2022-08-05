fe75078011669bbad9d944efbc84c76885686f91
username <youremail@domain.com>
2022-08-04 20:14:49 -0700

    all flight

--- controller/flights.js
+++ controller/flights.js
@@ -2,23 +2,39 @@ const Flight = require('../models/flight')
 
 module.exports = {
     new: newFlight,
-    create
+    create,
+    index
 }
 
 function newFlight(req, res){
-    res.render('flights/new.ejs')
+	res.render('flights/new.ejs')
 }
 
 function create(req, res){
-    // req.body.cast = req.body.cast.replace(/\s*,\s*/g, ',');
-  // split if it's not an empty string
-  if (req.body.cast) req.body.cast = req.body.cast.split(',');
-  const flight = new Flight(req.body);
-  flight.save(function(err) {
-    // one way to handle errors
-    if (err) return res.render('flights/new');
-    console.log(flight);
-    // for now, redirect right back to new.ejs
-    res.redirect('/flights/new');
-  });
+	// log out what the function needs
+	console.log(req.body)
+	// take teh contents of the form (req.body), and add it to our database
+	Flight.create(req.body, function(err, flightDoc){
+		if(err){
+			console.log(err, ' <- err in the flights create controller')
+			return res.render('flights/new.ejs')
+		}
+
+		console.log(flightDoc)
+		res.redirect('/flights')
+	})
 }
+
+function index(req, res){
+    //List out the flights
+    Flight.find({},function(err, allFlights){
+    console.log(allFlights)    
+    res.render('flights/index.ejs',{
+        flights: allFlights
+    });
+});
+    
+}
+
+
+
--- routes/flights.js
+++ routes/flights.js
@@ -3,6 +3,7 @@ var router = express.Router();
 const flightController = require('../controller/flights')
 
 /* GET users listing. */
+router.get('/',flightController.index);
 router.get('/new', flightController.new);
 router.post('/',flightController.create);
 
