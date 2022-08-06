c1daabc25f97b4bc182f5286033bb6024ee149d9
username <youremail@domain.com>
2022-08-06 14:34:15 -0700

    posts to /destinations

--- controller/flights.js
+++ controller/flights.js
@@ -9,7 +9,7 @@ module.exports = {
 
 function show(req, res) {
 	Flight.findById(req.params.id, function(err, flight) {
-		res.render('flights/show', {flight});
+		res.render('flights/show.ejs', {flight});
 	})
 }
 
