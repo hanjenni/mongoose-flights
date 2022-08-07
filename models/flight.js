ccd0cf7d0bcb338f5cb99af282215875c9125682
username <youremail@domain.com>
2022-08-07 14:59:34 -0700

    require flightNo

--- models/flight.js
+++ models/flight.js
@@ -9,8 +9,15 @@ const destinationSchema = new mongoose.Schema({
 const flightSchema = new mongoose.Schema({
     airline: String,
     airport: String,
-    flightNo: Number,
-    departs: { type: Date, default: Date.now },
+    flightNo: 
+        { type: Number, 
+            min: 10, 
+            max: 9999,
+            required: true}, 
+    departs: 
+        { type: Date, 
+            default: () => Date.now() + 7*24*60*60*1000,
+            required: true},
     destinations: [destinationSchema]
      
    
