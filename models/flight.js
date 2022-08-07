fa5d0f71ee3bfe2d7872cd21fdc3b4400717ddb5
username <youremail@domain.com>
2022-08-06 22:26:33 -0700

    destination redirect

--- models/flight.js
+++ models/flight.js
@@ -1,14 +1,8 @@
 const mongoose = require('mongoose')
 
 const destinationSchema = new mongoose.Schema({
-    airport: {
-        type: String, 
-        enum: ['ATL', 'DFW', 'DEN', 'LAX', 'SAN'],
-        default: 'DEN'},       
-    arrival: {
-        type: Date,
-        default: Date.now()
-    }  
+    airport: String,             
+    arrival:  Date,
 });
 
 
