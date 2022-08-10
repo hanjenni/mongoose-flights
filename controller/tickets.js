f3b808dcac14f122cdabd4bfac82039cdc827377
username <youremail@domain.com>
2022-08-09 17:11:30 -0700

    finished commit

--- controller/tickets.js
+++ controller/tickets.js
@@ -5,35 +5,43 @@ const Flight = require('../models/flight');
 module.exports = {
     new: newTicket,
     create,
-    addToFlight
+  
     
     
   
 }
 
-function addToFlight(req, res){
-    Flight.findById(req.params.id, function(err, flightDoc){
-        console.log(flight, '<--wheres the flight info?')
-        flightDoc.flight.push(req.body);
-        flightDoc.save(function(err){
-            res.redirect(`/flights/${req.params.id}`);
-        })
-    })
-}
 
 
 
-function create(req,res) {
-    console.log(create, '<-new ticket button')
+// function create(req,res) {
+//     console.log(create, '<-new ticket button')
+//     req.body.flight = req.params.id
+//     const ticket = new Ticket(req.body);
+//     ticket.save(function (err) {
+//         if (err) return res.render('tickets/new.ejs');
+//         res.redirect(`/flights/${ticket.flight}`);
+
+
+// });
+
+function create(req, res){
     req.body.flight = req.params.id
-    const ticket = new Ticket(req.body);
-    ticket.save(function (err) {
-        if (err) return res.render('tickets/new.ejs');
-        res.redirect(`/flights/${ticket.flight}`);
+    console.log(create, '<-new ticket button')
+    Ticket.create(req.body, function(err, ticket){
+        if(err) {
+            console.log('error')
+        }
+      
+        res.redirect(`/flights/${req.params.id}`)
+    });
+};
 
 
-});
-}
+
+
+
+
    
 function newTicket(req, res) {
     console.log(newTicket, '<-pressed buy new ticket')
--- routes/tickets.js
+++ routes/tickets.js
@@ -5,7 +5,7 @@ const ticketsCtrl = require('../controller/tickets');
 router.get("/flights/:id/tickets/new", ticketsCtrl.new);
 router.post("/flights/:id/tickets/new", ticketsCtrl.create);
 
-router.post('/flights/:id/tickets/new', ticketsCtrl.addToFlight);
+
 
 
 
